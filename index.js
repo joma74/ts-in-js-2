d5072b99840ba90e742be5749ae4f853afa14ba7
joma74 <dev-mails@gmx.net>
2018-09-10 00:47:41 +0200

    chore(typing): added second global access to shwo error

--- index.js
+++ index.js
@@ -12,10 +12,22 @@ var clazz1b = require("ts-in-js-1/clazz1b")
 var clazz1aInstance = new clazz1a();
 var clazz1bInstance = new clazz1b(clazz1aInstance);
 
-global.clazz1bGlobalVar= clazz1bInstance
+/**
+ * ISSUE 
+ * 1. Only on <reference types="ts-in-js-1/types/global" getErrorAsString and clazz1bGlobalVar are available for code completion
+ * 2. On global.getErrorAsString... , everything is fine with code completion; no error
+ * 3. But as soon as global.clazz1bGlobalVar is defined, global.getErrorAsString is marked as "...does not exist on type 'typeof global'"
+ * 4. After that, one can assign anything to clazz1bGlobalVar, even a string
+ */
+global.getErrorAsString(new Error("abc"))
+global.clazz1bGlobalVar="clazz1bInstance"
 
 var resultTsInJs1 = tsinjs1.clazz1bInstance.getExtensionFor("text/html");
 
+tsinjs1.clazz1bInstance.getErrorAsString(new Error("abc"))
+
 var resultTsInJs2 = clazz1bInstance.getExtensionFor("text/html");
+
+clazz1bInstance.getErrorAsString(new Error("abc"))
 
 module.exports={resultTsInJs1, resultTsInJs2}
